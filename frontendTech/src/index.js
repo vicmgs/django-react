ec42c3accd64a8d0daffc5324d042b827502b55f
Victor Mu <vic.mgs@gmail.com>
2017-07-25 23:08:40 -0700

    Beginner redux

--- frontendTech/src/index.js
+++ frontendTech/src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+const todos = (state = [], action) => {
+  switch (action.type){
+    case 'ADD_TODO':
+      return [...state, {
+        id: action.id,
+        text: action.text,
+        completed: false
+      }]
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo
+        }
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      })
+    default:
+      return state
+  }
+}
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <div>{ JSON.stringify(todos([{id: 'agasdf'}], {})) }</div>,
   document.getElementById('app')
-);
+)
