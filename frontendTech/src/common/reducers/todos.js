e8d2ed8e60408e2f2f29bf9158facd51e24f83fe
Victor Mu <vic.mgs@gmail.com>
2017-08-16 22:03:37 -0700

    pre wrapping dispatch 1

--- frontendTech/src/common/reducers/todos.js
+++ frontendTech/src/common/reducers/todos.js
@@ -1,44 +1,42 @@
-const todos = (state = [], action) => {
+import { combineReducers } from 'redux'
+import todo from './todo'
+
+const byIds = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state, todo(undefined, action)]
     case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action))
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action)
+      }
     default:
       return state
   }
 }
 
-const todo = (state, action) => {
+const allIds = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    case 'TOGGLE_TODO':
-        if (state.id !== action.id) {
-          return state
-        }
-        return {
-          ...state,
-          completed: !state.completed
-        }
+      return [...state, action.id]
     default:
       return state
   }
 }
+
+const todos = combineReducers({ byIds, allIds })
 
 export default todos
 
+const getAllTodos = (state) => state.allIds.map(id => state.byIds[id])
+
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state)
   switch (filter) {
     case 'all':
-      return state
+      return allTodos
     case 'completed':
-      return state.filter((t) => t.completed)
+      return allTodos.filter((t) => t.completed)
     case 'active':
-      return state.filter((t) => !t.completed)
+      return allTodos.filter((t) => !t.completed)
   }
 }
